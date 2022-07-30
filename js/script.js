45a69ad8ff7624b2008eb364d47b3c7d149465b4
IgorEverton <rm95214@fiap.com.br>
2022-07-30 14:21:14 -0300

    projeto concluido

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const elementoLigar=document.querySelector("#ligarId")
-const elementoDesLigar=document.querySelector("desligarId")
+const elementoDesLigar=document.querySelector("#desligarId")
 
 elementoLigar.addEventListener("click", ()=>{
     document.querySelector("#imgLamOffId").src="./img/lam_on.png"
